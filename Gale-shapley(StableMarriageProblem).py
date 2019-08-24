b0cbab1ec73af1ccddfea83df8f4f14394066555
Raj-Madheshia <rajsmadheshia@gmail.com>
2019-08-24 23:48:17 +0530

    SMP Gale-shapley problem

---
+++ Gale-shapley(StableMarriageProblem).py
@@ -0,0 +1,32 @@
+def matchfinding(males_ranking, female_ranking):\
+    # transpose for making calculation easy
+    males_ranking = [[males_ranking[j][i] for j in range(len(males_ranking))] for i in range(len(males_ranking[0]))] 
+    
+    l = len(males_ranking)
+    selected_males = [0]*l
+    assign_males =[-1]*l
+    for i_index,i in enumerate(males_ranking):
+        for index,j in enumerate(i):      
+            if not selected_males[index]:
+                if assign_males[j-1]==-1:
+                    assign_males[j-1] = index
+                    selected_males[index] =1
+                else:
+                    rank_by_female =female_ranking[j-1]
+                    print(assign_males[j-1])
+                    first = rank_by_female.index(assign_males[j-1])
+                    second = rank_by_female.index(index+1)
+                    if first > second :
+                        assign_males[j-1] =  index
+                        selected_males[assign_males[j-1]]=0
+                        selected_males[index] =1
+    for index, i in enumerate(assign_males):
+        print(index+1,">>>>>Match With<<<<<", i+1)
+        
+#a = [[1,3,4,3],[3,4,2,2],[2,1,3,1],[4,2,1,4]]
+a =[[1,3,2,4],[3,4,1,2],[4,2,3,1],[3,2,1,4]]
+b = [[2,1,3,4], [4,1,2,3], [1,3,2,4],[2,3,1,4]]
+
+a =[[1,2,3,4], [4,3,2,1],[1,2,4,3],[1,3,4,2]]
+b = [[3,1,4,2],[1,2,4,3],[1,2,3,4],[2,1,4,3]]
+matchfinding(a,b)
